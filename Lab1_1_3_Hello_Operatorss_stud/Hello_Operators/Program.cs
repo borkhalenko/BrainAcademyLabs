32566bd697a46588da15dbb950a4104867bb01b7
borkhalenko <borkhalenko@gmail.com>
2016-02-09 16:38:03 +0200

    Added new project Lab1_1_3_Hello_Operatorss_stud.

---
+++ Lab1_1_3_Hello_Operatorss_stud/Hello_Operators/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOperators_stud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a;
+            Console.WriteLine(@"Please,  type the number:
+            1. Farmer, wolf, goat and cabbage puzzle
+            2. Console calculator
+            3. Factirial calculation
+            ");
+            
+            a = long.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Farmer_puzzle();
+                    Console.WriteLine("");
+                    break;
+                case 2:
+                    Calculator();
+                    Console.WriteLine("");
+                    break;
+                case 3:
+                    Factorial_calculation();
+                    Console.WriteLine("");
+                    break;
+                default:
+                    Console.WriteLine("Exit");
+                    break;
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
+        }
+        #region farmer
+        static void Farmer_puzzle()
+        { 
+
+        }
+        #endregion
+
+        #region calculator
+        static void Calculator()
+        {
+           
+
+        }
+        #endregion
+
+        #region Factorial
+        static void Factorial_calculation()
+        {
+          
+        }
+        #endregion
+    }
+}
