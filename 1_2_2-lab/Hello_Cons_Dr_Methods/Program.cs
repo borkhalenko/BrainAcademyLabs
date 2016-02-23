7f1934997980715a7b546010a6c7c824c9e25fda
borkhalenko <borkhalenko@gmail.com>
2016-02-23 12:42:30 +0200

    Added 1_2_2-lab

---
+++ 1_2_2-lab/Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+           
+            Console.WriteLine("Press any key...");
+            Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!");
+            }
+
+
+            
+        }
+    }
+}
