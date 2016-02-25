9411dfed0beaf6f6ff637e4510d2151aebe49dca
borkhalenko <borkhalenko@gmail.com>
2016-02-25 21:26:55 +0200

    1_2_3-lab is done!

--- 1_2_3-lab/CSharp_Net-module1_2_3-lab/Money.cs
+++ 1_2_3-lab/CSharp_Net-module1_2_3-lab/Money.cs
@@ -1,32 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharp_Net_module1_2_3_lab
-{
+namespace CSharp_Net_module1_2_3_lab {
     // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
-    public enum CurrencyTypes { UAH, USD, EU};
+    public enum CurrencyTypes { UAH, USD, EU };
 
-    class Money
-    {
+    public class DifferentCurrencyTypesException: ApplicationException {
+        public DifferentCurrencyTypesException() { }
+        public DifferentCurrencyTypesException(string description) : base(description) { }
+        public DifferentCurrencyTypesException(string description, Exception innerException) : base(description, innerException) { }
+    }
+
+    public class AmountLessThanZeroException : ApplicationException {
+        public AmountLessThanZeroException() { }
+        public AmountLessThanZeroException(string description) : base(description) { }
+        public AmountLessThanZeroException(string description, Exception innerException) : base(description, innerException) { }
+    }
+
+    class Money {
         // 2) declare 2 properties Amount, CurrencyType
+        private Decimal amount;
+        public Decimal Amount {
+            get { return amount; }
+            set {
+                if (value < 0)
+                    //throw new ArgumentOutOfRangeException("value", value.ToString(), "The value must be greater than or equal to 0");
+                    throw new AmountLessThanZeroException("The value must be greater than or equal to 0");
+                amount = value;
+            }
+        }
+        public CurrencyTypes CurrencyType { get; private set; }
 
         // 3) declare parameter constructor for properties initialization
+        public Money(Decimal amount, CurrencyTypes currType = CurrencyTypes.UAH) {
+            Amount = amount;
+            CurrencyType = currType;
+        }
+
+        public Money(double amount, CurrencyTypes currType = CurrencyTypes.UAH) : this((decimal)amount, currType) { }
 
         // 4) declare overloading of operator + to add 2 objects of Money
+        public static Money operator+(Money op1, Money op2) {
+            if (op1.CurrencyType != op2.CurrencyType) {
+                throw new DifferentCurrencyTypesException("Operands have different currency type.");
+            }
+            return new Money(op1.Amount + op2.Amount, op1.CurrencyType); 
+        }
 
         // 5) declare overloading of operator -- to decrease object of Money by 1
+        public static Money operator--(Money op1) {
+            if (op1.Amount - 1 < 0) {
+                throw new AmountLessThanZeroException("operator--: result cannot be less then 0.");
+            }
+            return new Money(op1.Amount - 1, op1.CurrencyType);
+        }
 
         // 6) declare overloading of operator * to increase object of Money 3 times
+        public static Money operator*(Money op1, int op2) {
+            return new Money(op1.Amount * 3, op1.CurrencyType);
+        }
 
         // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator>(Money op1, Money op2) {
+            if (op1.CurrencyType != op2.CurrencyType) {
+                throw new DifferentCurrencyTypesException("Operands have different currency type.");
+            }
+            return op1.Amount > op2.Amount;
+        }
+
+        public static bool operator<(Money op1, Money op2) {
+            return op2 > op1;
+        }
 
         // 8) declare overloading of operator true and false to check CurrencyType of object
+        public static bool operator true(Money op1) {
+            return op1.Amount > 0;
+        }
+
+        public static bool operator false(Money op1) {
+            return op1.Amount <= 0;
+        }
 
         // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static implicit operator double(Money op1) {
+            return (double)op1.Amount;
+        }
 
+        public static explicit operator string(Money op1) {
+            return op1.Amount.ToString();
+        }
 
+        public static implicit operator Money(double op1) {
+            return new Money(new Decimal(op1));
+        }
+
+        public static explicit operator Money(string op1) {
+            return new Money(Decimal.Parse(op1));
+        }
+
+        public override string ToString() {
+            return Amount.ToString() + " " + CurrencyType.ToString();
+        }
     }
 }
--- 1_2_3-lab/CSharp_Net-module1_2_3-lab/Program.cs
+++ 1_2_3-lab/CSharp_Net-module1_2_3-lab/Program.cs
@@ -4,30 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp_Net_module1_2_3_lab
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace CSharp_Net_module1_2_3_lab {
+    class Program {
+        static void Main(string[] args) {
             // 10) declare 2 objects
+            Money m1 = new Money(2.3);
+            Money m2 = new Money(3.5M);
 
             // 11) do operations
             // add 2 objects of Money
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Adding m1:" + m1 + " and m2:" + m2);
+            Money mSum = m1 + m2;
+            Console.WriteLine(mSum.ToString());
+
+            Money m3 = new Money(4.0M, CurrencyTypes.USD);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Adding m1:" + m1 + " and m3:" + m3);
+            try {
+                Money res = m1 + m3;
+                Console.WriteLine(m3.ToString());
+            }
+            catch (DifferentCurrencyTypesException) {
+                Console.WriteLine("Ok. We're expecting for an exception.");
+            }
 
             // add 1st object of Money and double
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Adding m1:" + m1 + " to 3.2");
+            mSum = m1 + 3.2;
+            Console.WriteLine(mSum.ToString());
 
             // decrease 2nd object of Money by 1 
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("m2 before decreasing: " + m2.ToString());
+            Console.WriteLine("m2--");
+            Console.WriteLine(m2--);
+            Console.WriteLine("--m2");
+            Console.WriteLine(--m2);
 
             // increase 1st object of Money
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("m1 before increasing: " + m1.ToString());
+            Console.WriteLine("m1*3");
+            Console.WriteLine(m1 * 3);
 
             // compare 2 objects of Money
-
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Comparing: " + m1 + ">" + m2);
+            Console.WriteLine(m1 > m2);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Comparing: " + m1 + "<" + m2);
+            Console.WriteLine(m1 < m2);
             // compare 2nd object of Money and string
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Comparing: " + m1 + ">" + (Money)"2.5" + " (string)");
+            Console.WriteLine(m1 > (Money)"2.5");
 
             // check CurrencyType of every object
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Checking CurrencyType of m1:" + m1);
+            Console.WriteLine(m1.CurrencyType);
+
+            Console.WriteLine("Checking CurrencyType of m2:" + m2);
+            Console.WriteLine(m2.CurrencyType);
+
+            Console.WriteLine("Checking CurrencyType of m3:" + m3);
+            Console.WriteLine(m3.CurrencyType);
+
+            // testing true and false operators
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("m1:" + m1 + " is True?");
+            if (m1)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+            Money m4 = new Money(0M);
+
+            Console.WriteLine("m4:" + m4 + " is False?");
+            if (m4)
+                Console.WriteLine("No");
+            else
+                Console.WriteLine("Yes");
 
             // convert 1st object of Money to string
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Converting m1:" + m1 + " to string value:");
+            Console.WriteLine((string)m1);
 
         }
     }
